0b893341883f3db008ba173e4dc6b6c6d539b8f1
thereserayza <thereserayzaaaaaa@gmail.com>
2018-01-30 16:28:17 +0800

    Readded the logic for exposing mongodb Ids in repositoryconfig

--- src/main/java/com/ibm/ojt/RepositoryConfig.java
+++ src/main/java/com/ibm/ojt/RepositoryConfig.java
@@ -4,11 +4,17 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class RepositoryConfig {
-    
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+	
+    @Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Order.class);
+    }    
     public CustomConversions customConversions() {
 		return new CustomConversions(Arrays.asList(new StringToLocalDateTimeConverter()));
     }
